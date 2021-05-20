c3f0b1d0bd46aae73476a410bd06e6d2a28eb579
ShinJiHyeong <bs9992@nara.co.kr>
2021-05-20 10:13:05 +0900

    [main] BST Level-Order Traversal
    breadth-first search

---
+++ src/main/java/study/hacker_rank/tree/BSTLevelOrderTraversal.java
@@ -0,0 +1,51 @@
+package study.hacker_rank.tree;
+
+import java.util.LinkedList;
+
+import study.hacker_rank.Node;
+
+public class BSTLevelOrderTraversal {
+
+	static void levelOrder(Node node) {
+		if (node == null)
+			return;
+
+		LinkedList<Node> queue = new LinkedList<Node>();
+		queue.add(node);
+		while (!queue.isEmpty()) {
+			node = queue.poll();
+			System.out.print(node.data + " ");
+			if (node.left != null)
+				queue.add(node.left);
+			if (node.right != null)
+				queue.add(node.right);
+		}
+
+	}
+
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void main(String args[]) {
+		int[] array = new int[] { 3, 2, 5, 1, 4, 7 };
+
+		Node root = null;
+		for (int data : array) {
+			root = insert(root, data);
+		}
+		levelOrder(root);
+	}
+}
