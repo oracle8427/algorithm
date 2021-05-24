e7fcfc91989be50940086e6faae71608aa30365f
ShinJiHyeong <oracle8427@gmail.com>
2021-05-24 23:17:17 +0900

    [main] Running Time And Complexity. Prime number checker
    hackerrank
    Day 25: Running Time and Complexity

---
+++ src/main/java/study/hacker_rank/RunningTimeAndComplexity.java
@@ -0,0 +1,31 @@
+package study.hacker_rank;
+
+public class RunningTimeAndComplexity {
+
+	// O(root of number)
+	public static boolean isPrime(int number) {
+		if (number == 1)
+			return false;
+		int sqrt = (int) Math.sqrt(number);
+		for (int i = 2; i <= sqrt; i++) {
+			if (number % i == 0)
+				return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 12, 5, 7 };
+		// int[] array = { 1000000007, 100000003, 1000003 };
+		int size = array.length;
+
+		for (int i = 0; i < size; i++) {
+			int number = array[i];
+			if (isPrime(number))
+				System.out.println("Prime");
+			else
+				System.out.println("Not prime");
+		}
+
+	}
+}
