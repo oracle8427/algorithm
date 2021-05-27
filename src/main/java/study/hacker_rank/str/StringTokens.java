f19d25f9a4b9aa755fc54b1e9aee2c90d7baae4e
ShinJiHyeong <oracle8427@gmail.com>
2021-05-27 13:58:15 +0900

    [main] Java String Tokens
    https://www.hackerrank.com
    
    Given a string "S",  matching the regular expression [A-Za-z !,?._'@]+,
    split the string into tokens. We define a token to be one or more
    consecutive English alphabetic letters.
    Then, print the number of tokens, followed by each token on a new line.
    
    1 <= s.length() <= (4 * 10^5)
    
    String s is composed of any of the following:
    	English alphabetic letters, blank spaces, exclamation points (!),
    	commas (,), question marks (?), periods (.), underscores (_),
    	apostrophes ('), and at symbols (@).

---
+++ src/main/java/study/hacker_rank/str/StringTokens.java
@@ -0,0 +1,32 @@
+package study.hacker_rank.str;
+
+public class StringTokens {
+
+	public static void printTokens(String s) {
+		if (s == null || s.trim().length() == 0) {
+			System.out.print(0);
+			return;
+		}
+
+		String[] tokens = s.trim().split("[!?,._'@\\s]+");
+		System.out.println(tokens.length);
+		for (String token : tokens)
+			System.out.println(token);
+
+	}
+
+	public static void main(String[] args) {
+		String[] testCase = {
+			"He is a very very good boy, isn't he?",
+			"Hello, thanks for attempting this problem! Hope it will help you to learn java! Good luck and have a nice day!",
+			"           YES      leading spaces        are valid,    problemsetters are         evillllll",
+			"                        "
+		};
+
+		for (String s : testCase) {
+			printTokens(s);
+			System.out.println();
+		}
+	}
+
+}
