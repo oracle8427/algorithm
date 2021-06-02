e8961285fb84e101b971e09820a9ce617cb33e44
ShinJiHyeong <oracle8427@gmail.com>
2021-06-03 00:13:45 +0900

    [main] Java 2D Array
    https://www.hackerrank.com
    
    Constraints :
        6 X 6 2D Array
    
    a b c
      d
    e f g
    
    For example, if we create an hourglass using the number 1 within an
    array full of zeros, it may look like this:
    1 1 1 0 0 0
    0 1 0 0 0 0
    1 1 1 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    
    Actually, there are many hourglasses in the array above. The three
    leftmost hourglasses are the following:
    1 1 1     1 1 0     1 0 0
      1         0         0
    1 1 1     1 1 0     1 0 0
    
    The sum of an hourglass is the sum of all the numbers within it. The sum
    for the hourglasses above are 7, 4, and 2, respectively.
    
    In this problem you have to print the largest sum among all the
    hourglasses in the array.

---
+++ src/main/java/study/hacker_rank/list/HourglassSum.java
@@ -0,0 +1,40 @@
+package study.hacker_rank.list;
+
+import java.io.IOException;
+
+public class HourglassSum {
+
+	public static int hourglassSum(int[][] array) {
+		int maxSum = Integer.MIN_VALUE;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				int abc =	array[i][j] + 
+							array[i][j + 1] + 
+							array[i][j + 2];
+				int d 	 = 	array[i + 1][j + 1];
+				int efg =	array[i + 2][j] + 
+							array[i + 2][j + 1] + 
+							array[i + 2][j + 2];
+				int sum = abc + d + efg;
+				if (maxSum < sum)
+					maxSum = sum;
+			}
+		}
+		return maxSum;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		int[][] array = { 
+				new int[] { 1, 1, 1, 0, 0, 0 }, 
+				new int[] { 0, 1, 0, 0, 0, 0 },
+				new int[] { 1, 1, 1, 0, 0, 0 }, 
+				new int[] { 0, 0, 2, 4, 4, 0 }, 
+				new int[] { 0, 0, 0, 2, 0, 0 },
+				new int[] { 0, 0, 1, 2, 4, 0 } 
+		};
+
+		System.out.println(hourglassSum(array));
+	}
+
+}
